d40263a9c46e7a97475cadc1081121b41c68c514
XiaoYeTech <admin@xiaoyetech.team>
2025-03-19 20:55:49 +0800

    1

--- clash-rule-set/clash.py
+++ clash-rule-set/clash.py
@@ -1,21 +1,13 @@
 import os
 
 def add_prefix_suffix_to_file(filepath):
-    """
-    给指定文件的每一行添加前缀 "  - '" 和后缀 "'"。
-
-    Args:
-        filepath (str): 文件的路径。
-    """
     try:
-        # 读取文件内容
         with open(filepath, 'r', encoding='utf-8') as file:
             lines = file.readlines()
 
         # 修改每一行
         modified_lines = ["  - '" + line.rstrip() + "'\n" for line in lines]
 
-        # 将修改后的内容写回文件
         with open(filepath, 'w', encoding='utf-8') as file:
             file.writelines(modified_lines)
 
@@ -26,8 +18,6 @@ def add_prefix_suffix_to_file(filepath):
     except Exception as e:
         print(f"发生错误：{e}")
 
-# 获取文件路径
 file_path = input("请输入文件路径：")
 
-# 调用函数进行处理
 add_prefix_suffix_to_file(file_path)
---
+++ v2ray/v2ray.py
@@ -0,0 +1,23 @@
+import os
+
+def add_prefix_suffix_to_file(filepath):
+    try:
+        with open(filepath, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+
+        # 修改每一行
+        modified_lines = ["domain:" + line.rstrip() + ",\n" for line in lines]
+
+        with open(filepath, 'w', encoding='utf-8') as file:
+            file.writelines(modified_lines)
+
+        print(f"文件 {filepath} 已成功修改。")
+
+    except FileNotFoundError:
+        print(f"错误：文件 {filepath} 未找到。")
+    except Exception as e:
+        print(f"发生错误：{e}")
+
+file_path = input("请输入文件路径：")
+
+add_prefix_suffix_to_file(file_path)
